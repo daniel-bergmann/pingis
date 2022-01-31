ad93664f395c1fb7e00d15340c490bd18530a454
Daniel Bergmann <dba222@outlook.com>
2022-01-31 15:07:44 +0000

    x

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,45 +1,138 @@
 import { useState, useEffect } from 'react';
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
+import Image from 'next/image';
+
+import { search, mapImageResources, getFolders } from '../lib/cloudinary';
 
 import Layout from '@components/Layout';
 import Container from '@components/Container';
 import Button from '@components/Button';
 
-import images from '@data/images';
+import styles from '@styles/Home.module.scss';
 
-import styles from '@styles/Home.module.scss'
+export default function Home({
+  images: defaultImages,
+  nextCursor: defaultNextCursor,
+  folders,
+}) {
+  const [images, setImages] = useState(defaultImages);
+  const [nextCursor, setNextCursor] = useState(defaultNextCursor);
+  const [activeFolder, setActiveFolder] = useState('');
 
-export default function Home() {
+  async function handleLoadMore(e) {
+    e.preventDefault();
+    const results = await fetch('/api/search', {
+      method: 'POST',
+      body: JSON.stringify({
+        nextCursor,
+        expression: `folder="${activeFolder}"`,
+      }),
+    }).then((r) => r.json());
+    const { resources, next_cursor: updatedNextCursor } = results;
+
+    const images = mapImageResources(resources);
+
+    setImages((prev) => {
+      return [...prev, ...images];
+    });
+
+    setNextCursor(updatedNextCursor);
+  }
+
+  function handleOnFolderClick(e) {
+    const folderPath = e.target.dataset.folderPath;
+    setActiveFolder(folderPath);
+    setNextCursor(undefined);
+    setImages([]);
+  }
+
+  useEffect(() => {
+    (async function run() {
+      const results = await fetch('/api/search', {
+        method: 'POST',
+        body: JSON.stringify({
+          nextCursor,
+          expression: `folder="${activeFolder}"`,
+        }),
+      }).then((r) => r.json());
+      const { resources, next_cursor: updatedNextCursor } = results;
+
+      const images = mapImageResources(resources);
+
+      setImages((prev) => {
+        return [...prev, ...images];
+      });
+
+      setNextCursor(updatedNextCursor);
+    })();
+  }, [activeFolder]);
+
   return (
     <Layout>
       <Head>
         <title>My Images</title>
-        <meta name="description" content="All of my cool images." />
+        <meta name='description' content='All of my cool images.' />
       </Head>
 
       <Container>
-        <h1 className="sr-only">My Images</h1>
+        <h1 className='sr-only'>My Images</h1>
 
+        <h2 className={styles.header}>Folders</h2>
+
+        <ul className={styles.folders} onClick={handleOnFolderClick}>
+          {folders.map((folder) => {
+            return (
+              <li key={folder.path}>
+                <button data-folder-path={folder.path}>{folder.name}</button>
+              </li>
+            );
+          })}
+        </ul>
+        <p>
+          <Button onClick={handleLoadMore}>Load More Results</Button>
+        </p>
         <h2 className={styles.header}>Images</h2>
 
         <ul className={styles.images}>
-          {images.map(image => {
+          {images.map((image) => {
             return (
               <li key={image.id}>
-                <a href={image.link} rel="noreferrer">
+                <a href={image.link} rel='noreferrer'>
                   <div className={styles.imageImage}>
-                    <Image width={image.width} height={image.height} src={image.image} alt="" />
+                    <Image
+                      width={image.width}
+                      height={image.height}
+                      src={image.image}
+                      alt=''
+                    />
                   </div>
-                  <h3 className={styles.imageTitle}>
-                    { image.title }
-                  </h3>
+                  <h3 className={styles.imageTitle}>{image.title}</h3>
                 </a>
               </li>
-            )
+            );
           })}
         </ul>
       </Container>
     </Layout>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const results = await search({
+    expression: 'folder=""',
+  });
+  // next cursor is used so the images wont be sent all at once from the server but instead first some and then you can click "next page" for more
+  const { resources, next_cursor: nextCursor } = results;
+
+  const images = mapImageResources(resources);
+
+  const { folders } = await getFolders();
+
+  return {
+    props: {
+      images,
+      nextCursor: nextCursor || false,
+      folders,
+    },
+  };
 }
