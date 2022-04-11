c552f1c82db5dc866ecb90d11232a547f9efe950
Daniel Bergmann <61001914+daniel-bergmann@users.noreply.github.com>
2022-04-11 11:33:59 +0000

    fixed a bug on server side

--- src/lib/cloudinary.js
+++ src/lib/cloudinary.js
@@ -35,33 +35,33 @@ export async function search(options = {}) {
 // ++++++++++++++++++++++++++++++++++++++++++++++++
 // Fetching folders for when the app will be scaled later
 
-// export async function getFolders(options = {}) {
-//   const results = await fetch(
-//     `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/folders`,
-//     {
-//       headers: {
-//         Authorization: `Basic ${Buffer.from(
-//           process.env.CLOUDINARY_API_KEY +
-//             ':' +
-//             process.env.CLOUDINARY_API_SECRET
-//         ).toString('base64')}`,
-//       },
-//     }
-//   ).then((r) => r.json());
+export async function getFolders(options = {}) {
+  const results = await fetch(
+    `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/folders`,
+    {
+      headers: {
+        Authorization: `Basic ${Buffer.from(
+          process.env.CLOUDINARY_API_KEY +
+            ':' +
+            process.env.CLOUDINARY_API_SECRET
+        ).toString('base64')}`,
+      },
+    }
+  ).then((r) => r.json());
 
-//   return results;
-// }
+  return results;
+}
 
-// export function mapImageResources(resources) {
-//   return resources.map((resource) => {
-//     // destructuring height and width straight from resource
-//     const { width, height } = resource;
-//     return {
-//       id: resource.asset_id,
-//       title: resource.public_id,
-//       image: resource.secure_url,
-//       width,
-//       height,
-//     };
-//   });
-// }
+export function mapImageResources(resources) {
+  return resources.map((resource) => {
+    // destructuring height and width straight from resource
+    const { width, height } = resource;
+    return {
+      id: resource.asset_id,
+      title: resource.public_id,
+      image: resource.secure_url,
+      width,
+      height,
+    };
+  });
+}
