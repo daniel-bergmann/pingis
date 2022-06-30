485ccb05a3e61a63e090b6bffe90f3dd384730e4
loft <loft@lofts-MacBook-Air.local>
2022-06-30 12:17:07 +0000

    too much animation, hit the sweet spot

--- src/components/button.js
+++ src/components/button.js
@@ -31,6 +31,7 @@ const ButtonStyling = styled.div`
   button {
     height: 10px;
     width: 150px;
+    margin-bottom: 30px;
   }
   @media (min-width: 768px) {
     button {
@@ -38,7 +39,7 @@ const ButtonStyling = styled.div`
       width: 160px;
     }
     &:hover {
-      opacity: 0.9;
+      opacity: 0.8;
       /* Start the shake animation and make the animation last for 0.5 seconds */
       animation: shake 0.5s;
 
@@ -50,36 +51,10 @@ const ButtonStyling = styled.div`
       0% {
         transform: translate(1px, 1px) rotate(0deg);
       }
-      /* 10% {
-        transform: translate(-1px, -2px) rotate(-1deg);
-      }
-      20% {
-        transform: translate(-3px, 0px) rotate(1deg);
-      } */
+
       30% {
         transform: translate(3px, 2px) rotate(0deg);
       }
-      40% {
-        transform: translate(1px, -1px) rotate(1deg);
-      }
-      50% {
-        transform: translate(-1px, 2px) rotate(-1deg);
-      }
-      60% {
-        transform: translate(-3px, 1px) rotate(0deg);
-      }
-      70% {
-        transform: translate(3px, 1px) rotate(-1deg);
-      }
-      80% {
-        transform: translate(-1px, -1px) rotate(1deg);
-      }
-      /* 90% {
-        transform: translate(1px, 2px) rotate(0deg);
-      }
-      100% {
-        transform: translate(1px, -2px) rotate(-1deg);
-      } */
     }
   }
 `;
