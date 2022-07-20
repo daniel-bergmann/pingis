9dec522c5ee4d801bed483002bf01c3bb40d8503
daniel-bergmann <dba222@outlook.com>
2022-07-20 12:24:36 +0000

    fixed react icons bug

--- src/components/layout/scrollToTop.js
+++ src/components/layout/scrollToTop.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { GrLinkTop } from "react-icons";
+
 
 const ScrollToTop = () => {
   const [showTopBtn, setShowTopBtn] = useState(false);
